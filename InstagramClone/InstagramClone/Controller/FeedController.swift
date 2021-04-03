214c1ddb5e56583072f1e16dec8bb7156056929e
Kim Min Seop <msk7529@naver.com>
2021-04-04 00:16:47 +0900

    탭바컨트롤러에 뷰컨들 연결

---
+++ InstagramClone/InstagramClone/Controller/FeedController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedController.swift
+//  InstagramClone
+//
+//  Created by kakao on 2021/04/04.
+//
+
+import UIKit
+
+final class FeedController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ InstagramClone/InstagramClone/Controller/SearchController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchController.swift
+//  InstagramClone
+//
+//  Created by kakao on 2021/04/04.
+//
+
+import UIKit
+
+final class SearchController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
