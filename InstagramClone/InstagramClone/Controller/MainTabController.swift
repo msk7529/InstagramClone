c01ccdfa9a648fcb3eafb80a7786809ddf14034f
Kim Min Seop <msk7529@naver.com>
2021-04-04 00:01:25 +0900

    탭바컨트롤러 생성 및 폴더 생성

---
+++ InstagramClone/InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by kakao on 2021/04/03.
+//
+import UIKit
+
+final class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
