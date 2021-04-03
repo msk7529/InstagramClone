3fdfca81d554258091d667147111dc6aecb47b26
Kim Min Seop <msk7529@naver.com>
2021-04-04 00:43:16 +0900

    탭바 이미지파일 추가 및 네비게이션컨트롤러 연결

--- InstagramClone/InstagramClone/Controller/MainTabController.swift
+++ InstagramClone/InstagramClone/Controller/MainTabController.swift
@@ -20,12 +20,21 @@ final class MainTabController: UITabBarController {
     private func configureViewControllers() {
         self.view.backgroundColor = .systemBackground
         
-        let feedVC: FeedController = .init()
-        let searchVC: SearchController = .init()
-        let imageSelectorVC: ImageSelectorController = .init()
-        let notificationVC: NotificationController = .init()
-        let profileVC: ProfileController = .init()
+        let feedVC: UINavigationController = templateNavigationController(unselectedImg: UIImage(named: "home_unselected")!, selectedImg: UIImage(named: "home_selected")!, rootVC: FeedController())
+        let searchVC: UINavigationController = templateNavigationController(unselectedImg: UIImage(named: "search_unselected")!, selectedImg: UIImage(named: "search_selected")!, rootVC: SearchController())
+        let imageSelectorVC: UINavigationController = templateNavigationController(unselectedImg: UIImage(named: "plus_unselected")!, selectedImg: UIImage(named: "plus_unselected")!, rootVC: ImageSelectorController())
+        let notificationVC: UINavigationController = templateNavigationController(unselectedImg: UIImage(named: "like_unselected")!, selectedImg: UIImage(named: "like_selected")!, rootVC: NotificationController())
+        let profileVC: UINavigationController = templateNavigationController(unselectedImg: UIImage(named: "profile_unselected")!, selectedImg: UIImage(named: "profile_selected")!, rootVC: ProfileController())
         
         self.viewControllers = [feedVC, searchVC, imageSelectorVC, notificationVC, profileVC]
+        self.tabBar.tintColor = .black
+    }
+    
+    private func templateNavigationController(unselectedImg: UIImage, selectedImg: UIImage, rootVC: UIViewController) -> UINavigationController {
+        let navigationVC: UINavigationController = .init(rootViewController: rootVC)
+        navigationVC.tabBarItem.image = unselectedImg
+        navigationVC.tabBarItem.selectedImage = selectedImg
+        navigationVC.navigationBar.tintColor = .black
+        return navigationVC
     }
 }
