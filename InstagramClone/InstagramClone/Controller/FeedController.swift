02c02093780412b67f88a83747f0ebd8b794b7f7
Kim Min Seop <msk7529@naver.com>
2021-04-04 01:16:26 +0900

    FeedVC - collectionVC로 변경, 기본틀 잡기

--- InstagramClone/InstagramClone/Controller/FeedController.swift
+++ InstagramClone/InstagramClone/Controller/FeedController.swift
@@ -7,10 +7,40 @@
 
 import UIKit
 
-final class FeedController: UIViewController {
+final class FeedController: UICollectionViewController {
+    private static let cellIdentifier: String = "feedCell"
+    
+    // - MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBlue
+        self.configureUI()
+    }
+    
+    // - MARK: Helpers
+    private func configureUI() {
+        collectionView.backgroundColor = .systemBackground
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: Self.cellIdentifier)
+    }
+}
+
+// - MARK: UICollectionViewDataSource
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: Self.cellIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// - MARK: UICollectionViewFlowLayout
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
 }
