10378657d9351119f32e6bd8bc2edd9660f185de
Kim Min Seop <msk7529@naver.com>
2021-04-05 00:21:24 +0900

    RegistrationVC - Sign Up 화면 추가

--- InstagramClone/InstagramClone/Controller/Authentication/RegistrationController.swift
+++ InstagramClone/InstagramClone/Controller/Authentication/RegistrationController.swift
@@ -10,13 +10,91 @@ import UIKit
 final class RegistrationController: UIViewController {
     
     // - MARK: Properties
+    private let plusPhotoButton: UIButton = {
+        let button: UIButton = .init(type: .system)
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let emailTextField: CustomTextField = {
+        let textField: CustomTextField = .init(placeholder: "Email")
+        textField.keyboardType = .emailAddress
+        return textField
+    }()
+    
+    private let passwordTextField: CustomTextField = {
+        let textField: CustomTextField = .init(placeholder: "Password")
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let fullnameTextField: CustomTextField = .init(placeholder: "Fullname")
+    private let usernameTextField: CustomTextField = .init(placeholder: "Username")
 
+    private let signUpButton: UIButton = {
+        let button: UIButton = .init(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView: UIStackView = .init(arrangedSubviews: [emailTextField, passwordTextField, fullnameTextField, usernameTextField, signUpButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let alreadyHaveAccountButton: UIButton = {
+        let button: UIButton = .init(type: .system)
+        button.attributedTitle(firstPart: "Already have an account?", secondPart: "Log In")
+        button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     // - MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .systemPink
+        self.configureUI()
     }
     
     // - MARK: Helpers
+    private func configureUI() {
+        self.view.backgroundColor = .systemPink
+        navigationController?.navigationBar.isHidden = true     // true가 기본속성값
+        navigationController?.navigationBar.barStyle = .black   // 네비게이션바에 light tint를 적용한다.
+        
+        self.configureGradientLayer()
+        
+        self.view.addSubview(plusPhotoButton)
+        self.view.addSubview(stackView)
+        self.view.addSubview(alreadyHaveAccountButton)
+        
+        plusPhotoButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 32).isActive = true
+        stackView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 32).isActive = true
+        stackView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -32).isActive = true
+        
+        alreadyHaveAccountButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        alreadyHaveAccountButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    // - MARK: Actions
+    @objc private func handleShowLogin() {
+        navigationController?.popViewController(animated: true)
+    }
 }
