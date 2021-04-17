06a6a44796f96e6339f50e039e4ee23e92b44dc9
Kim Min Seop <msk7529@naver.com>
2021-04-18 07:07:10 +0900

    SearchController을 TableViewController로 변경, 기본 UI 세팅

--- InstagramClone/InstagramClone/Controller/SearchController.swift
+++ InstagramClone/InstagramClone/Controller/SearchController.swift
@@ -7,10 +7,35 @@
 
 import UIKit
 
-final class SearchController: UIViewController {
+final class SearchController: UITableViewController {
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemPink
+        configureTableView()
+    }
+    
+    // MARK: - Helpers
+    private func configureTableView() {
+        view.backgroundColor = .systemBackground
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: UserCell.identifier)
+        tableView.rowHeight = 64
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SearchController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: UserCell.identifier, for: indexPath) as? UserCell else {
+            return UITableViewCell()
+        }
+        return cell
     }
 }
